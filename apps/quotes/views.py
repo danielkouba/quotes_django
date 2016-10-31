ef64ef904ba00b47b3dbfb6c797a668d7345288d
Daniel Kouba <koubaphotog@gmail.com>
2016-10-30 17:16:26 -0700

    super styled and feeling good

--- apps/quotes/views.py
+++ apps/quotes/views.py
@@ -6,14 +6,17 @@ from .models import User, Quote, Favorite
 # Create your views here.
 def index(request):
 	if 'logged_user' not in request.session:
+		messages.error(request, 'Please Sign in to see more!')
 		return redirect('/')
 
 	user = User.objects.get(id = request.session['logged_user'])
+	alias = user.alias
 	allposts = Quote.objects.exclude(favorite__favoritedby = user)
 	favorites = Favorite.objects.filter(favoritedby = user)
 	context = {
 		'allposts': allposts,
-		'favorites': favorites
+		'favorites': favorites,
+		'alias': alias
 	}
 	return render(request, 'quotes/index.html', context)
 
@@ -22,12 +25,14 @@ def addQuote(request):
 	post = request.POST
 	user = User.objects.get(id = request.session['logged_user'])
 	Quote.objects.create(postedby=user, quotedby=post['quotedby'], message=post['message'])
+	messages.success(request, 'You successfully added a quote!')
 	return redirect('quotes:index')
 
 def addFavorite(request,quoteid):
 	post = Quote.objects.get(id = quoteid)
 	user = User.objects.get(id = request.session['logged_user'])
 	Favorite.objects.create(favoritequote = post, favoritedby = user)
+	messages.success(request, 'You Successfully added a quote to your favorites!')
 	return redirect('quotes:index')
 
 def removeFavorite(request, quoteid):
@@ -36,6 +41,10 @@ def removeFavorite(request, quoteid):
 	return redirect('quotes:index')
 
 def showUser(request, userid):
+	if 'logged_user' not in request.session:
+		messages.error(request, 'Please Sign in to see more!')
+		return redirect('/')
+
 	user = User.objects.get(id = userid)
 	allposts = Quote.objects.filter(postedby = user)
 	alias = user.alias
